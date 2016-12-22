e78319451458a6ec81108e1438014ec936ccdbb6
Sean Feil <sean@tophatandmonocle.com>
2016-12-21 20:47:12 -0700

    Skills

--- application/views/skills.php
+++ application/views/skills.php
@@ -1,11 +1,323 @@
 
 <main id="skills">
 	<div class="container center-headers container-topadjust">
-		<section id="primaryskills">
+		<h1>Skills and proficiencies</h1>
+		<section id="skillsexplination">
+			<p>A bunch of skills and proficiencies I have which can be checked off an HR checklist. Click a skill to get a brief example of what I've done with it.</p>
 
 		</section>
-		<section id="secondaryskills">
+		<section id="codeskills">
+			<h2>Programming languages and frameworks</h2>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>c#</h3>
+						<ul>
+							<li>Framework versions 2, 4, 4.5</li>
+							<li>ASP.NET MVC 4</li>
+							<li>nHibernate ORM</li>
+							<li>NUnit</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Java</h3>
+						<ul>
+							<li>Spring Framework</li>
+							<li>JUnit</li>
+						</ul>
+					</div>
+				</div>
 
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>PHP</h3>
+						<ul>
+							<li>CodeIgniter MVC</li>
+							<li>Laravel MVC</li>
+							<li>Eloquent ORM</li>
+							<li>PHPUnit</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>JavaScript</h3>
+						<ul>
+							<li>Vue.JS</li>
+							<li>jQuery</li>
+							<li>NodeJS</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>HTML and CSS</h3>
+						<ul>
+							<li>SASS</li>
+							<li>Twitter Bootstrap</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Scripting languages</h3>
+						<ul>
+							<li>BASH</li>
+							<li>PowerShell</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>PeopleSoft</h3>
+						<ul>
+							<li>PeopleCode 8.5x</li>
+							<li>Integration broker</li>
+							<li>Interaction hub (portal) module</li>
+							<li>Human Capital Management module (9.x)</li>
+							<li>Campus Solutions module(9.x)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</section>
+
+
+		<section id="softwaretechnologies">
+
+			<h2>Software technologies</h2>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Databases</h3>
+						<ul>
+							<li>SQL-based (MySQL, Oracle SQL, SQL Server)</li>
+							<li>NoSQL-based (CouchDB)</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Version control</h3>
+						<ul>
+							<li>GIT</li>
+							<li>Subversion (SVN)</li>
+							<li>CVS</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Web services access</h3>
+						<ul>
+							<li>REST-based</li>
+							<li>SOAP-based</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Testing automation</h3>
+						<ul>
+							<li>Selenium</li>
+							<li>jQuery</li>
+							<li>NodeJS</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Performance and analytics</h3>
+						<ul>
+							<li>New Relic</li>
+							<li>Google Analytics</li>
+							<li>Elastic stack (formally ELK stack)</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Platforms and service hosting</h3>
+						<ul>
+							<li>Red Hat Enterprise Linux (RHEL 6, 7)</li>
+							<li>Windows Server (2003, 2008, 2012, 2016)</li>
+							<li>Amazon Web Services (AWS)</li>
+							<li>Microsoft Azure</li>
+							<li>Docker</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="Technologyconcepts">
+
+			<h2>Applications</h2>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Federated identity</h3>
+						<ul>
+							<li>Shibboleth</li>
+							<li>Azure Active Directory Premium</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Unfederated single sign-on</h3>
+						<ul>
+							<li>Central Authentication Service (CAS)</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Directory services</h3>
+						<ul>
+							<li>Active Directory on-premise</li>
+							<li>OpenLDAP</li>
+							<li>UNITIS (proprietary)</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Learning management solutions</h3>
+						<ul>
+							<li>Blackboard</li>
+							<li>Moodle</li>
+							<li>Desire2Learn</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Content management systems</h3>
+						<ul>
+							<li>Drupal</li>
+							<li>WordPress</li>
+							<li>Joomla</li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section id="organizational">
+
+			<h2>Organizational</h2>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Project management methodologies</h3>
+						<ul>
+							<li>Agile methodology</li>
+							<li>Waterfall methodology</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>User testing</h3>
+						<ul>
+							<li>User acceptance testing (UAT)</li>
+							<li>A/B testing</li>
+							<li>Usability testing</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Design principles</h3>
+						<ul>
+							<li>Responsive web design</li>
+							<li>User-centered design</li>
+							<li>Test-driven development</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="handy">
+
+			<h2>Other handy things</h2>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Office skills</h3>
+						<ul>
+							<li>Microsoft Office (Word, Excel, PowerPoint, Project, Visio, Access)</li>
+							<li>Apple iWork / iCloud</li>
+							<li>Google Docs</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Organizational tools</h3>
+						<ul>
+							<li>SharePoint</li>
+							<li>TRAC</li>
+							<li>Project Server</li>
+							<li>Remedy Action Request System (ARS)</li>
+							<li>ServiceNow</li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Client operating systems</h3>
+						<ul>
+							<li>Windows (starting with 3.x)</li>
+							<li>MacOS (formally OS X)</li>
+							<li>Ubuntu desktop</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="skillbox">
+						<h3>Client tools</h3>
+						<ul>
+							<li>Sublime Text 2</li>
+							<li>Emacs</li>
+							<li>VIM</li>
+							<li>Scrivener</li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
 		</section>
 
 	</div>
